62b5bbd62c020ddcf0f82e7b1fc4f7c6d0a194f0
Thomas Morgenstern <tjmorgee@gmail.com>
2023-04-13 15:16:19 -0500

    Fixed build path and module-info

--- GroupProject2ICS372/edu.ics372.gp2.train/module-info.java
+++ GroupProject2ICS372/edu.ics372.gp2.train/module-info.java
@@ -2,9 +2,6 @@ module edu.ics372.gp2.train {
 	requires javafx.controls;
 	requires javafx.base;
 	requires java.desktop;
-	requires javafx.graphics;
-
-	opens edu.ics372.gp2.train.start to javafx.graphics, javafx.fxml;
 
 	exports edu.ics372.gp2.train.display to javafx.graphics;
 	exports edu.ics372.gp2.train.start to javafx.graphics;
